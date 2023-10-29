3aaa8da5e1269b05da16ec7c2df333f2a5480ddb
hclulu <hclulu@hotmail.com>
2023-10-29 14:19:13 +0800

    fix: toggler img改用svg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -37,7 +37,7 @@ watch(menuSelected, (newMenuSelected) => {
 
 <template>
   <button class="toggler" @click="showMenu = true">
-    <img src="menu.svg" alt="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
   </button>
 
   <nav class="sidebar" :class="{ show: showMenu }" v-on-click-outside="closeMenu">
